64ec470f3fadda52cf12156db3e9f6867ea1f4f3
willklep <william.klepinger@ymail.com>
2021-03-23 13:43:56 -0400

    Updated HuntingMinigame, Wagon, Weather; Ready for JavaDoc

--- src/HuntingMinigame.java
+++ src/HuntingMinigame.java
@@ -1,3 +1,21 @@
 public class HuntingMinigame {
 
+    // Vars
+    private int animalsRemaining;
+
+    /**
+     * Constructor for HuntingMinigame class
+     */
+    public HuntingMinigame(){
+        ;
+    }
+
+    /**
+     * Function to simulate gameplay
+     * @return true if player is playing game
+     * false otherwise
+     */
+    public Boolean playGame(){
+        return true;
+    }
 }
--- src/Wagon.java
+++ src/Wagon.java
@@ -24,8 +24,76 @@ public class Wagon {
     /**
      * Updates food for player
      */
-    public void updateFood(){
+    private void updateFood(){
         ;
     }
 
+    /**
+     * Check if end of trail
+     */
+    private void checkEnd(){
+        ;
+    }
+
+    /**
+     * gets amount of money
+     * @return money
+     */
+    public int getMoney() {
+        return money;
+    }
+
+    /**
+     * Sets players money
+     * @param money
+     */
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    /**
+     * adds food to player's wagon
+     */
+    public void addFood(int food){
+        ;
+    }
+
+    /**
+     *
+     * @return speed of wagon in relation to how many oxen they have
+     */
+    public int getSpeed(){
+        return 0;
+    }
+
+    /**
+     * repairs wagon by setting health to 100
+     */
+    public void repairWagon(){
+        health = 100;
+    }
+
+    /**
+     * Adds ammo to wagon
+     * @param ammo - amount of ammo to be added to player's current ammo
+     */
+    public void addAmmo(int ammo){
+        this.ammo += ammo;
+    }
+
+    /**
+     * removes ammo from wagon
+     * @param ammo - amount of ammo to be removed to player's current ammo
+     */
+    public void removeAmmo(int ammo){
+        this.ammo -= ammo;
+    }
+
+    /**
+     * retrieve ammo in wagon
+     * @returns wagon's current ammo
+     */
+    public int getAmmo() {
+        return ammo;
+    }
 }
