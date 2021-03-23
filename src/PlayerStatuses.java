b150c078f7c9c220c50a3d9a948464ef1e3e55a6
Dominic Hupp <dhupp2020@gmail.com>
2021-03-23 12:24:36 -0400

    Created Javadoc comments for player statuses

--- src/PlayerStatuses.java
+++ src/PlayerStatuses.java
@@ -1,2 +1,19 @@
+//*******************************************************************
+// PlayerStatuses
+//
+// Holds the enumeration for the player states to determine character health
+//
+// Date Last Updated: 3.23.21
+// Author(s): Dominic Hupp
+//*******************************************************************
+
+
 public enum PlayerStatuses {
+    HEALTHY,
+    FATIGUED,
+    SICK_CHOLERA,
+    SICK_MALARIA,
+    SICK_SMALLPOX,
+    FREEZING,
+    DEAD
 }
