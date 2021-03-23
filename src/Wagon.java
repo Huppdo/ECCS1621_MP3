906026349a60056c3998c463e6d1825f27abc22f
willklep <william.klepinger@ymail.com>
2021-03-23 13:09:56 -0400

    Edited HuntingMinigame, Wagon, Weather

--- src/Wagon.java
+++ src/Wagon.java
@@ -1,2 +1,31 @@
+/**
+ * Represents the player’s wagon.
+ *
+ * @authors Will Klepinger
+ * Last updated: 03/23/21
+ */
 public class Wagon {
+    char[] family;
+    int money;
+    int food;
+    int oxAmount;
+    int spareParts;
+    int ammo;
+    int health;
+
+    /**
+     * Constructor for Wagon class
+     */
+    // constructor
+    public Wagon(){
+        ;
+    }
+
+    /**
+     * Updates food for player
+     */
+    public void updateFood(){
+        ;
+    }
+
 }
